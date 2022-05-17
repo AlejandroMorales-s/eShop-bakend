6c4b2a2b8cdcd6bbc11cf9f711e6ddc416b82ae0
Alejandro <alejandrom.am33@gmail.com>
2022-05-16 22:33:14 -0500

    add: endpoint for all products

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
+//* Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const { application } = require('express');
+const prisma = new PrismaClient();
+
 app.get('/', (req, res) => {
   res.json({message: 'eShop API'});
+});
+
+//* Endpoint para listar todos los produtos
+app.get('/products', async (req, res) => {
+    const allProducts =  await prisma.Products.findMany({});
+    res.json(allProducts);
 });
 
 app.listen(port, () => {
